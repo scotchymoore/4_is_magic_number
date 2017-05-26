7335c7ff680fe905e9fb9dec4371b14cf0d370f8
Scott Moore <scotchymoore@gmail.com>
2017-05-26 15:52:12 -0600

    change num to word

--- w2bt.rb
+++ w2bt.rb
@@ -1,10 +1,12 @@
 require 'numbers_and_words'
 
 
+
 def num_to_string(numbers)
   num_word = numbers.to_words remove_hyphen: true
   num_value = num_word.length.to_i
-  puts "#{numbers} is equal to #{num_value}."
+  num_val_word = num_value.to_words
+  puts "#{numbers} is equal to #{num_val_word}."
   if num_value != 4
     numbers = num_value
     num_to_string(numbers)
